faed1632e383f9a795607232a8e140a72bb8199c
tqkoh <shktmygs@gmail.com>
2023-03-04 06:28:56 +0900

    fix
    https

--- src/Const.hpp
+++ src/Const.hpp
@@ -5,7 +5,7 @@
 #define DEV 0
 
 #if DEV == 0
-#define API_URL "wss://tqk.trap.show/snowball-server/"
+#define API_URL "wss://tqk.trap.show/snowball-server/api/ws"
 #elif DEV == 1
 #define API_URL "ws://dev.snowball-server.tqk.trap.show/api/ws"
 #elif DEV == 2
